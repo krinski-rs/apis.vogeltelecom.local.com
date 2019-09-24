b50b669eedb2c6c2696e6931766af1e22cfb5fd2
Reinaldo Freitas <reinaldo.freitas@vogeltelecom.com>
2019-09-24 09:04:34 -0300

    ajuste para não commitar prod

--- src/Services/IntegracaoProtheus/Pedido/PedidosProtheus.php
+++ src/Services/IntegracaoProtheus/Pedido/PedidosProtheus.php
@@ -46,7 +46,7 @@ class PedidosProtheus
     public function __construct(EntityManager $objEntityManager, Logger $objLogger)
     {
         $this->objEntityManager = $objEntityManager;
-        $this->objLogger                = $objLogger;
+        $this->objLogger        = $objLogger;
     }
     
     /**
@@ -67,14 +67,12 @@ SELECT
 	,E1_NUMBOR AS		'bordero'
 	,E1_PORTADO AS		'Portador(banco)'
 	,E1_PREFIXO AS		'Prefixo'
-	,Convert(varchar(10),cast(E1_BAIXA as date),103) AS		'baixa(Protheus)'
+    ,CASE WHEN E1_BAIXA <> '' THEN Convert(varchar(10),cast(E1_BAIXA as date),103) ELSE NULL END AS 'baixa(Protheus)'
 	,Convert(varchar(10),cast(E1_VENCORI as date),103) AS  'Data_vencimento_origem(Protheus)'
 	,Convert(varchar(10),cast(E1_VENCTO as date),103) AS 		'Data_vencimento(Protheus)'
 	,E1_DESCONT AS		'Desconto'
 	,F2.F2_DOC
-	
 FROM  SE1V50 AS E1
-
 INNER JOIN SF2V50 AS F2 ON E1.E1_P_REF = F2.F2_P_REF
 WHERE E1_P_REF <> '' AND RTRIM(E1_P_REF) = '{$pedido}';
 SQL;
@@ -110,10 +108,7 @@ SELECT
 	,Convert(varchar(10),cast(E1_VENCTO as date),103) AS 		'Data_vencimento(Protheus)'
 	,E1_DESCONT AS		'Desconto'
 	,F2.F2_DOC
-
-
 FROM  SE1V50 AS E1
-
 INNER JOIN SF2V50 AS F2 ON E1.E1_P_REF = F2.F2_P_REF
 WHERE E1_P_REF <> '' AND RTRIM(E1_P_REF) IN ({$a}) ORDER BY RTRIM(E1_P_REF);
 SQL;
